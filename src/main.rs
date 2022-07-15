efcd20bb32d3a4b6181a2e870f886c500d370b37
Natan Freeman <natanfreeman@outlook.com>
2022-07-15 15:11:04 +0300

    implements fix discussed in https://github.com/rustwasm/wasm-bindgen/issues/2992

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use gloo_utils::{document, window};
+use core::result::Result;
+use gloo_utils::document;
+use wasm_bindgen::JsCast;
+use web_sys::{CssStyleRule, CssStyleSheet};
 use yew::prelude::*;
 
 #[function_component(Main)]
 pub fn main(_props: &()) -> Html {
-    let root = document().query_selector(":root").unwrap().unwrap();
-
-    let style = window().get_computed_style(&root).unwrap().unwrap();
-
-    let progress = style.get_property_value("--color").unwrap();
-    println!("{progress}");
-
-    style.set_property("--color", "blue").unwrap();
+    let style_sheet = document().style_sheets().item(0).unwrap();
+    let css_style_object: Result<CssStyleSheet, _> = style_sheet.dyn_into();
+    let rule: CssStyleRule = css_style_object
+        .unwrap()
+        .css_rules()
+        .unwrap()
+        .item(0)
+        .unwrap()
+        .dyn_into()
+        .unwrap();
+    let style = rule.style();
+    style.set_property("--color", "black").unwrap();
 
     return html! {
         <>
